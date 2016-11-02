e748492e075e45e178b68373853496b8c8ccdedd
Pranavgulati <pranavgulati95@yahoo.com>
2016-11-02 19:04:53 +0530

    made example

--- neuralDuino/neuralDuino.ino
+++ neuralDuino/neuralDuino.ino
@@ -9,8 +9,13 @@ neuralDuino node1;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+	Serial.begin(115200);
+	//these are normalized inputs
 	float input[NUM_SYN] = { 0.5, 0.6, 0.8, 0.9, 0.5 };
-
+	node1.getOutput(input);
+	for (byte i = 0; i < 10; i++){
+		node1.adjustWeights(5, 0.1);
+	}
 }
 
 // the loop function runs over and over again until power down or reset
